c2e6a2360e526bec72375438e9c0c0a3c78f7646
Amit Roy <roy932439@gmail.com>
2024-06-22 21:59:32 +0530

    first commit

---
+++ src/components/Header/Playlist.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+export default function Playlist() {
+  return (
+    <nav className="flex">
+      <ul>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            My Playlist
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Haluk and ErenYalcn
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            In the Car +200km/h
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Haluk Levent List
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Instrumental Music
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Hip Hop America
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Rock Classics
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Chill Vibes
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Workout Pump-Up
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Jazz Lounge
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Country Roads
+          </a>
+        </li>
+        <li className="mb-2">
+          <a
+            href="#"
+            className="text-link hover:text-white text-sm font-semibold"
+          >
+            Electronic Dance Party
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
